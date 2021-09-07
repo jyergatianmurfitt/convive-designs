f8f727ef912b2b8390a1dc704851550d2a2d9a4a
Josephina <jyergatianmurfitt@googlemail.com>
2021-09-07 18:43:41 +0100

    added scroll and load animations

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Navbar//
 function toggleDropdown() {
   let navbarToggle = document.getElementById('navbar-toggle');
   if (navbarToggle.className === 'topNav') {
@@ -7,3 +8,42 @@ function toggleDropdown() {
     navbarToggle.className = 'topNav';
   }
 }
+
+//OnLoad Animations//
+const animateHome = () => {
+  const homeContainer = document.querySelector('.header-container');
+
+  setTimeout(function(){
+      homeContainer.style.opacity = '1';
+  }, 0);
+}
+
+//Scroll Animations//
+const scrollElements = document.querySelectorAll(".scroll");
+
+const elementInView = (el, scrollOffset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    ((window.innerHeight || document.documentElement.clientHeight) - scrollOffset)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 100)) {
+      displayScrollElement(el);
+    }
+  })
+}
+
+setTimeout(function(){
+  window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+  })
+}, 0);
